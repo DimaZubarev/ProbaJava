1687250b2f98c9044880b8d685fe0fd40f4b222e
Dima <1983zubarev@gmail.com>
2017-03-13 17:33:06 +0200

    Виправлення в задачі 2 тижня

--- src/prometheus/week2/BinarySearch2_5my.java
+++ src/prometheus/week2/BinarySearch2_5my.java
@@ -2,7 +2,7 @@ package prometheus.week2;
 
 public class BinarySearch2_5my {
     public static void main(String[] args) {
-        int data[] = { 3, 6, 7, 10, 34, 56, 60 };
+        int data[] = {3, 6, 7, 10, 34, 56, 60};
         int numberToFind = 10;
         int i = -1;
         int count = -1;
